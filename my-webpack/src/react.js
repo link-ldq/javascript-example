c0dc4013c826d359c032999f61b59cdcb1732e2a
lidaqing04 <lidaqing04@meituan.com>
2022-07-31 17:27:44 +0800

    css less

--- my-webpack/src/react.js
+++ my-webpack/src/react.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import './index.css'
+import './index.less'
 class Rc extends React.Component{
   render() {
     return <div>hello react</div>
