d80143d11629b147ef416b1be798f82f88173125
link <link0627@163.com>
2022-10-31 12:53:44 +0800

    fix

--- README.md
+++
@@ -1,7 +0,0 @@
-# javascript 实例，对标语雀和灵题库的学习
-
-记录我的八股文学习日常
-
-[在线文档](https://www.yuque.com/u2402848/ab30za)
-
-[语雀主页](https://www.yuque.com/u2402848)
---
+++ vue/数据双向绑定实现/vue.test.js
@@ -0,0 +1,8 @@
+
+import { expect, assert, describe, it, test } from 'vitest';
+
+describe('NodeList', () => {
+  it('isEmpty', () => {
+    expect(2).toBe(2)
+  })
+})
