6e99cf6e4b4425ad9171e5435df07efaa221c4d2
link <link0627@163.com>
2022-12-19 14:51:28 +0800

    fix

--- angular/ng-matero/src/app/routes/llr/phm/phm.service.ts
+++ angular/ng-matero/src/app/routes/llr/phm/phm.service.ts
@@ -3,11 +3,11 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 const TABLIST = [
   {
-    url: 'http://127.0.0.1:8050',
+    url: 'http://localhost:8050/report/a',
     title: '关键性能参数'
   },
   {
-    url: 'http://127.0.0.1:8050',
+    url: 'http://localhost:8050/report/a',
     title: 'test'
   },
 ]
